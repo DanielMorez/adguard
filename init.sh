648e487582e0591f35f5dd950262fc4de7f708ba
denisduginov <d.duginov@etpgpb.ru>
2026-03-09 23:05:44 +0400

    prod ready

--- init.sh
+++ init.sh
@@ -159,15 +159,12 @@ dns:
   trusted_proxies:
     - 127.0.0.0/8
     - ::1/128
-    - 172.16.0.0/12
   protection_enabled: true
   filtering_enabled: true
   blocking_mode: default
   blocked_response_ttl: 10
 tls:
-  enabled: true
-  certificate_path: /opt/adguardhome/certs/live/${DOMAIN}/fullchain.pem
-  private_key_path: /opt/adguardhome/certs/live/${DOMAIN}/privkey.pem
+  enabled: false
 querylog:
   enabled: true
   file_enabled: true
