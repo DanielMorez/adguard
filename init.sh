fa33b1f12c56ae80bc330d9968f716a45692b7de
denisduginov <d.duginov@etpgpb.ru>
2026-03-09 23:26:13 +0400

    prod ready

--- init.sh
+++ init.sh
@@ -165,8 +165,8 @@ dns:
   blocked_response_ttl: 10
 tls:
   enabled: true
-  certificate_path: /opt/adguardhome/certs/live/adguard.choomba.tech/fullchain.pem
-  private_key_path: /opt/adguardhome/certs/live/adguard.choomba.tech/privkey.pem
+  certificate_path: /opt/adguardhome/certs/live/${DOMAIN}/fullchain.pem
+  private_key_path: /opt/adguardhome/certs/live/${DOMAIN}/privkey.pem
 querylog:
   enabled: true
   file_enabled: true
